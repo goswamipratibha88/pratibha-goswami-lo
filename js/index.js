7c1b72db5d93dbbac53e644e92e9473381107f1b
goswamipratibha88 <168401181+goswamipratibha88@users.noreply.github.com>
2024-09-24 17:39:35 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,24 +1,23 @@
 
+// Code for creating footer
+
 let footer = document.createElement('footer');
+footer.className = "footer";
 document.body.appendChild(footer);
 const today = new Date();
 const thisYear = today.getFullYear();
 console.log(thisYear);
-
 footer = document.querySelector('footer');
-
 let copyright = document.createElement('p');
-
-copyright.innerHTML = `© Pratibha Goswami ${thisYear}`;
-
-
+copyright.innerHTML = `© ${thisYear} Pratibha Goswami `;
 footer.appendChild(copyright);
 
 
-const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
+// Code for adding skills in portfolio skills section
 
-let skillsSection = document.querySelector("#skills");
-let skillsList = document.querySelector("#skills > ul");
+const skills = ["JavaScript", "HTML", "CSS", "GitHub", "SVN", "JAVA", "VSCode", "Eclipse", "XML", "Selenium", "SQL", "HP ALM", "JIRA"];
+let skillsSection = document.querySelector("#Skills");
+let skillsList = document.querySelector("#Skills > ul");
 
 for (let i = 0; i < skills.length; i++) {
     let skill = document.createElement("li");
@@ -26,8 +25,45 @@ for (let i = 0; i < skills.length; i++) {
     skillsList.appendChild(skill);
 }
 
+// Code for adding git hub repos in portfolio project section 
+
+fetch('https://api.github.com/users/goswamipratibha88/repos')
+    .then((response) => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            throw new Error('Failed  to fetch');
+        }
+    })
+    .then((data) => {
+        const repositories = JSON.parse(data);
+        console.log(repositories);
+
+        let projectSection = document.getElementById("Projects");
+        let projectList = document.createElement("ul");
+        projectSection.appendChild(projectList);
+
+
+        for (let i = 0; i < repositories.length; i++) {
+            let li = document.createElement("li");
+
+            let a = document.createElement("a");
+            a.href = 'https://github.com/goswamipratibha88/' + repositories[i]["name"];
+            a.textContent = repositories[i]["name"];
+            li.appendChild(a);
+            projectList.appendChild(li);
+
+        }
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error.message);
+    })
+
+
+//Code for sending message in 'Leave a Message' section and viewing messages
+
 let messageForm = document.getElementById("leave_message");
-    messageForm.addEventListener('submit', function(event){
+messageForm.addEventListener('submit', function (event) {
     event.preventDefault();
     let name = event.target.usersName.value;
     let email = event.target.usersEmail.value;
@@ -37,20 +73,17 @@ let messageForm = document.getElementById("leave_message");
     console.log('Email: ', email);
     console.log('Message: ', userMessage);
 
-    let messageSection = document.getElementById('messages');
+    let messageSection = document.getElementById('viewmessages');
     let messageList = messageSection.querySelector('ul');
-
     let newMessage = document.createElement("li");
 
     newMessage.innerHTML = `
-    <a href="mailto:${email}">${name}</a>: 
-    <span>${userMessage}</span>
-`;
-    
-let removeButton = document.createElement("button");
-removeButton.innerText ="Remove";
-        removeButton.type ="button";
-    removeButton.addEventListener('click', function(){
+    <a href="mailto:${email}">${name}</a>   :  <span>${email}</span>  :  <span>${userMessage}</span>`;
+
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.type = "button";
+    removeButton.addEventListener('click', function () {
         let entry = removeButton.parentNode;
         entry.remove();
 
@@ -60,8 +93,4 @@ removeButton.innerText ="Remove";
     messageList.appendChild(newMessage);
     messageForm.reset();
 
-    });
-
-
-    // index.js:59 Uncaught DOMException: Failed to execute 'appendChild' on 'Node': This node type does not support this method.
-    // at HTMLFormElement.<anonymous> (file:///Users/pratibhagoswami/CodeVS/pratibha-goswami-lo/js/index.js:59:16)
+});
